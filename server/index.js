011a51bfc3486e015dd55fafe371bda221d5e32a
Sourav Mitra <sourav.mitra4679@gmail.com>
2026-04-11 12:56:46 +0530

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000
 connectDB()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true
 }))
 app.use(express.json())
